6b27a058027e6ce728476baa3ae6b82898a887ce
Parker Lillywhite <parkerlillywhite@gmail.com>
2024-09-14 17:14:48 -0600

    adds basic navbar

---
+++ src/navigation/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Col, Row, Container } from 'react-bootstrap';
+
+const navbar = () => {
+    return(
+        <Container>
+            <Row className="navbar-container">
+
+            </Row>
+        </Container>
+    )
+}
+
+export default navbar;
