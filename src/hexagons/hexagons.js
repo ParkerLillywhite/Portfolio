7e86cf8b3f1b76f9d20e09a92fa944a000ba22ca
Parker Lillywhite <parkerlillywhite@gmail.com>
2024-09-14 18:42:02 -0600

    hexagon base

---
+++ src/hexagons/hexagons.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container } from 'react-bootstrap'
+import './hexagons.css'
+
+const Hexagons = () => {
+    return(
+        <div className="hexrow">
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Hexagons
